2e69e7f161478faf0f67cfb2a5d801e342f0a77e
Demet AKDOĞAN <demet.akd@hotmail.com>
2023-12-31 13:40:15 +0300

    reactready

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -1,14 +1,7 @@
 ﻿using Business.Abstract;
-using Business.Concrete;
-using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
 {
@@ -34,7 +27,7 @@ namespace WebAPI.Controllers
 
             Thread.Sleep(1000);
 
-            var result =  _productService.GetAll();
+            var result = _productService.GetAll();
             if (result.Success)
             {
                 return Ok(result);
